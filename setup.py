ff0e324707765ff74d5f606c4b370aef8d71c1fa
Kamen <kamenpetrov014@gmail.com>
2023-10-08 21:33:07 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eb-nlp',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies required by your project here
+    ],
+)
