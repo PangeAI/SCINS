3695486b6c821b7579062b7ea53d76c8acf79694
Kamen Petrov <kamenpetrov014@gmail.com>
2024-07-20 16:49:44 +0300

    remove import from __init__

--- scins/__init__.py
+++
@@ -1,5 +0,0 @@
-from .scins import mol_to_num_chain_assemblies
-from .scins import mol_to_chain_lengths
-from .scins import mol_to_rings
-from .scins import mol_to_num_ring_assemblies
-# from .scins import GetScaffoldForMol_edited
